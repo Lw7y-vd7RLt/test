66edccbd4243e8c24f7973bbbe1cec6bca2c91bb
cheburashka <cheburashka@yandex.ru>
2017-11-15 20:23:33 +0400

    Create base structure

---
+++ src/index.js
@@ -0,0 +1,10 @@
+var $ = require('jquery');
+var str = require('./constant');
+
+var App = function() {
+    console.log(str);
+    $('body').html(src);
+};
+
+var app = new App();
+app.get = function() {};
